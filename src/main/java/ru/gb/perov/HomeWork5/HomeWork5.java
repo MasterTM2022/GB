160aaa00dfc5180f31ef4812733fcdce27a6de81
MasterTM2022 <perov.public@gmail.com>
2022-05-26 01:23:24 +0300

    add HomeWork5

---
+++ src/main/java/ru/gb/perov/HomeWork5/HomeWork5.java
@@ -0,0 +1,33 @@
+package ru.gb.perov.HomeWork5;
+
+public class HomeWork5 {
+    public static void main(String[] args) {
+        Person[] persArray = new Person[5];
+        persArray[0] = new Person("Ivanov Ivan Ivanovich", "Engineer", "ivan_ivanov@mailbox.com", "84951111111", 30000, 30);
+        persArray[1] = new Person("Petrov Petr Petrovich", "Chief Engineer", "petr_petrov@mailbox.com", "84952222222", 40000, 45);
+        persArray[2] = new Person("Sidorov Sidor Sidorovich", "Bodyguard", "sidor_sidorov@mailbox.com", "84953333333", 100000, 55);
+        persArray[3] = new Person("Olgina Olga Olegovna", "Director", "olga_olgina@mailbox.com", "84954444444", 99000, 18);
+        persArray[4] = new Person("Evgenieva Evgehia Evgenievna", "Account", "evgenia_evgenieva@mailbox.com", "84955555555", 98000, 19);
+
+        listOld(persArray);
+        listOldByMe(persArray);
+    }
+// метод, вызывающий стандартный override toString()
+    public static void listOld(Person[] array) {
+        for (Person person : array) {
+            if (person.getAge() > 40) {
+                System.out.println(person);
+            }
+        }
+        System.out.println();
+    }
+
+// но была задача "Внутри класса «Сотрудник» написать метод...", поэтому написал...
+    public static void listOldByMe(Person[] array) {
+        for (Person person : array) {
+            if (person.getAge() > 40) {
+                System.out.println(person.personToString());
+            }
+        }
+    }
+}
---
+++ src/main/java/ru/gb/perov/HomeWork5/Person.java
@@ -0,0 +1,91 @@
+package ru.gb.perov.HomeWork5;
+
+public class Person {
+    private String FIO;
+    private String position;
+    private String email;
+    private String phone;
+    private int salary;
+    private int age;
+
+    public Person(String FIO, String position, String email, String phone, int salary, int age) {
+        this.FIO = FIO;
+        this.position = position;
+        this.email = email;
+        this.phone = phone;
+        this.salary = salary;
+        this.age = age;
+    }
+
+    public void setSalary(int salary) {
+        salary = salary < 0 ? Math.abs(salary) : salary;
+        this.salary = salary;
+    }
+
+    public void setEmail(String email) {
+        email = email.contains("@") ? email : "";
+        email = email.contains(".") ? email : "";
+        this.email = email;
+    }
+
+    public void setAge(int age) {
+        age = age < 0 ? Math.abs(age) : age;
+        age = Math.min(age, 150);
+        this.age = age;
+    }
+
+    public String getFIO() {
+        return FIO;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void printPerson (Person person) {
+        System.out.println("Фамилия, имя, отчество: " + person.FIO);
+        System.out.println("Должность: " + person.position);
+        System.out.println("E-mail: " + person.email);
+        System.out.println("Телефон: " + person.phone);
+        System.out.println("Зарплата: " + person.salary);
+        System.out.println("Возвраст: " + person.age);
+    }
+    public String personToString() {
+        return "Сотрудник{" +
+                "ФИО='" + this.FIO + '\'' +
+                ", должность='" + position + '\'' +
+                ", email='" + email + '\'' +
+                ", телефон='" + phone + '\'' +
+                ", зарплата=" + salary +
+                ", возврат=" + age +
+                '}';
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "FIO='" + FIO + '\'' +
+                ", position='" + position + '\'' +
+                ", email='" + email + '\'' +
+                ", phone='" + phone + '\'' +
+                ", salary=" + salary +
+                ", age=" + age +
+                '}';
+    }
+}
