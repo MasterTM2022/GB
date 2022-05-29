dc42b0cab61d90d76b53df3fcaca10c4c48631ef
MasterTM2022 <perov.public@gmail.com>
2022-05-29 07:48:32 +0300

    add HomeWork7

---
+++ src/main/java/ru/gb/perov/HomeWork7/Main.java
@@ -0,0 +1,51 @@
+package ru.gb.perov.HomeWork7;
+
+public class Main {
+    public static void main(String[] args) {
+        Cat[] catHouse = {
+                new Cat("Barsik", 5, true),
+                new Cat("Chernysh", 6, true),
+                new Cat("Plyuha", 8, true),
+                new Cat("Alisa", 7, true),
+                new Cat("Murchik", 4, true)
+        };
+        Plate[] plates = {
+                new Plate(10, 120),
+                new Plate(2, 120),
+                new Plate(5, 120)
+        };
+
+        System.out.println("Pets before eating:");
+        for (Cat cat : catHouse) {
+            System.out.println(cat);
+        }
+
+        System.out.println("Plates before eating:");
+        for (int i = 0; i < plates.length; i++) {
+            System.out.println("№" + (i+1)+ " " + plates[i].info());
+        }
+
+        for (Cat cat : catHouse) {
+            int numberPlate = (int) (Math.random() * plates.length);
+            while (!cat.isSatiety()) {
+                if (cat.eat(plates[numberPlate])) {
+                    System.out.println(cat.getName() + " decrease food in plate " + (numberPlate+1) + " (-" + cat.getAppetite() + ")");
+                    System.out.println("next cat...");
+                    break;
+                } else {
+                    plates[numberPlate].increaseFood();
+                }
+            }
+        }
+
+        System.out.println("Pets after eating:");
+        for (Cat cat : catHouse) {
+            System.out.println(cat);
+        }
+
+        System.out.println("Plates after eating:");
+        for (int i = 0; i < plates.length; i++) {
+            System.out.println("№" + (i+1)+ " " + plates[i].info());
+        }
+    }
+}
---
+++ src/main/java/ru/gb/perov/HomeWork7/Plate.java
@@ -0,0 +1,44 @@
+package ru.gb.perov.HomeWork7;
+
+import java.util.Scanner;
+
+public class Plate {
+    private int food;
+    private final int limitFood;
+    int LIMIT_PLATE = 30;
+
+    public Plate(int food, int limitFood) {
+        this.food = food;
+        this.limitFood = LIMIT_PLATE;
+    }
+    public String info() {
+        return "plate: " + food;
+    }
+
+    public boolean decreaseFood(int n) {
+        if (food < n) {
+            return false;
+        } else {
+            food -= n;
+            return true;
+        }
+    }
+
+    public void increaseFood() {
+        System.out.println("Master, add some food in plate, please (no more than " + (limitFood - food) + ")");
+        Scanner in = new Scanner(System.in);
+        int addedFood;
+        do {
+            addedFood = in.nextInt();
+            if (addedFood <= 0) {
+                System.out.println("Master??? We didn't understand, meow...");
+            } else if (addedFood + food > limitFood) {
+                System.out.println("Master, so much... Our plate is very small for your bounty... A little smaller, meow...");
+            } else {
+                System.out.println("Master, you are so kind to us, meow...");
+                food = addedFood + food;
+            }
+        } while (!(addedFood > 0 && food <= limitFood));
+
+    }
+}
