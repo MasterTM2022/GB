da32b0c8df9106bf2a27b47899133c7b039edc88
MasterTM2022 <perov.public@gmail.com>
2022-06-08 20:03:01 +0300

    add part2.HomeWork2

--- src/main/java/ru/gb/perov/Part2/HomeWork2/MyException/NumberFormatException.java
+++
@@ -1,7 +0,0 @@
-package ru.gb.perov.Part2.HomeWork2.MyException;
-
-public class NumberFormatException extends Exception {
-    public NumberFormatException (String message) {
-        super(message);
-    }
-}
